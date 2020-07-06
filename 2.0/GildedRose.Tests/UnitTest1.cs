f0b7b514f0ee8d3027ea09b228b3a408c1f974a3
Joe <joeridgley@labstats.com>
2020-07-05 18:59:17 -0600

    Added inventory service, updated framework of console app to 4.7

---
+++ 2.0/GildedRose.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GildedRose.Tests
+{
+    [TestClass]
+    public class InventoryTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ 2.0/Inventory/ClientService/Inventory.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using InventoryService.Common;
+
+namespace InventoryService.ClientService
+{
+    public class Inventory
+    {
+        private List<Item> _items;
+
+        public Inventory(List<Item> Items)
+        {
+            _items = Items;
+        }
+
+        public void UpdateQuality()
+        {
+            for (var i = 0; i < _items.Count; i++)
+            {
+                if (_items[i].Name != "Aged Brie" && _items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                {
+                    if (_items[i].Quality > 0)
+                    {
+                        if (_items[i].Name != "Sulfuras, Hand of Ragnaros")
+                        {
+                            _items[i].Quality = _items[i].Quality - 1;
+                        }
+                    }
+                }
+                else
+                {
+                    if (_items[i].Quality < 50)
+                    {
+                        _items[i].Quality = _items[i].Quality + 1;
+
+                        if (_items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
+                        {
+                            if (_items[i].SellIn < 11)
+                            {
+                                if (_items[i].Quality < 50)
+                                {
+                                    _items[i].Quality = _items[i].Quality + 1;
+                                }
+                            }
+
+                            if (_items[i].SellIn < 6)
+                            {
+                                if (_items[i].Quality < 50)
+                                {
+                                    _items[i].Quality = _items[i].Quality + 1;
+                                }
+                            }
+                        }
+                    }
+                }
+
+                if (_items[i].Name != "Sulfuras, Hand of Ragnaros")
+                {
+                    _items[i].SellIn = _items[i].SellIn - 1;
+                }
+
+                if (_items[i].SellIn < 0)
+                {
+                    if (_items[i].Name != "Aged Brie")
+                    {
+                        if (_items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                        {
+                            if (_items[i].Quality > 0)
+                            {
+                                if (_items[i].Name != "Sulfuras, Hand of Ragnaros")
+                                {
+                                    _items[i].Quality = _items[i].Quality - 1;
+                                }
+                            }
+                        }
+                        else
+                        {
+                            _items[i].Quality = _items[i].Quality - _items[i].Quality;
+                        }
+                    }
+                    else
+                    {
+                        if (_items[i].Quality < 50)
+                        {
+                            _items[i].Quality = _items[i].Quality + 1;
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+}
